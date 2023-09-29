789ea24de029325e5b400168c12d17a07e136a20
siti nurviatika <114561174+nurviatika@users.noreply.github.com>
2023-09-29 19:54:48 +0700

    Add files via upload

---
+++ tugassesi1.js
@@ -0,0 +1,10 @@
+//Identitas
+const nama = "Siti Nurviatika";
+const nim = "20220040281";
+const tahunLahir = 2004;
+const tahunSekarang = 2023;
+const umur = tahunSekarang - tahunLahir;
+
+console.log(`NAMA : ${nama}`);
+console.log(`NIM : ${nim}`);
+console.log(`UMUR : ${umur}`);
